8162db3ff0ae14d92c460d522dfa3359ad948122
Sohail <sohailmomin1@gmail.com>
2017-06-21 14:36:05 +0530

    sets up the app configuration

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const morgan = require('morgan');
 const app = express();
 
 //app setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+
+//nodemon runs without the script
 
 //server setup
 const PORT = process.env.port || 3000;
