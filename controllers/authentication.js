0b43702b702cc4d6be36af315b9f7e6b44f2b0a3
Sohail <sohailmomin1@gmail.com>
2017-06-22 16:02:09 +0530

    adds token for exchange with user ID

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,15 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config')
+
+function tokenForUser(user) {
+	const timestamp = new Date().getTime();
+	return jwt.encode({
+		sub: user.id,
+		ait: timestamp 
+	}, 
+	config.secret)
+}
 
 exports.signup = function(req, res, next) {
 	const email = req.body.email;
@@ -22,7 +33,7 @@ exports.signup = function(req, res, next) {
 
 		user.save(function(err){
 			if(err) {return next(err);}
-			res.send({success: true});
+			res.send({token: tokenForUser(user)});
 		});
 	});
 }
